8d6372004b58db33bb55f7c4b1084d7575bd23cc
TehAutumnCore <naruto13581@gmail.com>
2025-04-22 15:17:30 -0400

    hello world

---
+++ Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// For .NET 5 and earlier, the console app template generates the following code: 
+// dotnet new console
+
+// using System;
+
+// namespace MyApp
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+using System;
+
+namespace HelloWorld // Namespace declaration
+{
+    class Program // Class declaration
+    {
+        static void Main(string[] args) // Main method declaration, entry point for the program. The array of strings are whatever the command line arguements that the app was started with.
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string str = Console.ReadLine();
+            Console.WriteLine("Why, hello there " + str);
+        }
+        
+    }
+    
+}
+// Console.WriteLine("Hello, World!");
