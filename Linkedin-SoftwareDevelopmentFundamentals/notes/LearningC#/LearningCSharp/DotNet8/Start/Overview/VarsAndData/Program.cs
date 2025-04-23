9814e6e409e20a5ec21fec475b4ef445260e9fb0
TehAutumnCore <naruto13581@gmail.com>
2025-04-23 13:12:19 -0400

    Variables and datatypes

--- Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Overview/VarsAndData/Program.cs
+++ Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Overview/VarsAndData/Program.cs
@@ -21,19 +21,28 @@ namespace VarsAndData
             var z = "Hello!";
 
             // TODO: Declare an array of values
-
+            int[] values = new int[5];
+            string[] strs = {"one","two","three"};
 
             // TODO: Print the values using a Formatting String
-
+            // Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}", i,c,b,str,f,d,x,z);
+            // Console.WriteLine($"int:{i}, char:{c}, bool:{b}, str:{str}, float:{f}, decimal:{d},var x: {x}, var y:{y}");
 
             // TODO: "null" means "no value"
-
+            //will print an empty line because there is nothing
+            object obj = null;
+            // Console.WriteLine(obj);
 
             // TODO: Implicit conversion between types
-
+            long bignum;
+            bignum = i;
 
             // TODO: Explicit conversions
+            float i_to_f = (float)i;
+            Console.WriteLine("{0}",i_to_f);
 
+            int f_to_i = (int)f;
+            Console.WriteLine("{0}",f_to_i);
         }
     }
 }
