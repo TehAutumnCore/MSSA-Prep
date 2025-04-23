b7fb8a61cc4a818afa2d1734bd87ee128b7a6507
TehAutumnCore <naruto13581@gmail.com>
2025-04-23 15:26:14 -0400

    operators

--- Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Overview/Operators/Program.cs
+++ Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Overview/Operators/Program.cs
@@ -12,23 +12,31 @@ namespace Operators
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
-
+            // Console.WriteLine((x / y) * x);
+            // Console.WriteLine(a + b);
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
-
+            x++;
+            y--;
+            // Console.WriteLine(x);
+            // Console.WriteLine(y);
 
             // TODO: Operators can be shorthand: a = a + b
-
+            // a += b; // a = a + b
+            // Console.WriteLine(a);
 
             // TODO: Logical operators &&, ||
             Console.WriteLine("----- Logic Operators -----");
-
+            // Console.WriteLine(x > y && y >= 5); // True if x is greater than y and y>= 5
+            // Console.WriteLine(x > y || y >= 5); // True if x is greater than y or y >= 5
 
             // null-coalescing operators
-            string str = null;
+            // string str = null; //is null
+            // string str = "hello"; //is not null
             // TODO: the ?? operator uses left operand if not null, or right one if it is
-
+            // If its not null itll print the left operand and if it is null then prints the right operand
+            Console.WriteLine(str ?? "Unknown string");
             
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
@@ -36,6 +44,9 @@ namespace Operators
             //    variable = somevalue;
             // }
 
+            //assigns a value only if the variable is currently null.
+            str ??= "New String";
+            Console.WriteLine(str);
         }
     }
 }
