eee4bb985464c3bf21e8c6ec0c4fa6353ea6af1d
TehAutumnCore <naruto13581@gmail.com>
2025-04-30 21:51:27 -0400

    string interpolation and stringbuilder

--- Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Strings/Interpolation/Program.cs
+++ Linkedin-SoftwareDevelopmentFundamentals/notes/LearningC#/LearningCSharp/DotNet8/Start/Strings/Interpolation/Program.cs
@@ -14,13 +14,17 @@ namespace StringInterpolation
             decimal price = 60_275.0m;
 
             // TODO: Output information using formatting
-
+            Console.WriteLine("This car is a {0} {1} {2}. with {3} miles and costs ${4}",
+            year, make, model, miles, price);
+            // This car is a 2020 Mercedes-Benz G Class. with 8450.27 miles and costs $60275.0
 
             // TODO: Using string interpolation
-
+            Console.WriteLine($"This car is a {year} {make} {model}, with {miles} miles and costs {price:C2}");
+            // This car is a 2020 Mercedes-Benz G Class, with 8450.27 miles and costs $60,275.00
 
             // TODO: With inline expressions
-
+            Console.WriteLine($"This car is a {year} {make} {model}, with {{{miles * 1.6:F2}}} km and costs {price:C2}");
+            // This car is a 2020 Mercedes-Benz G Class, with {13520.43} km and costs $60,275.00
         }
     }
 }
