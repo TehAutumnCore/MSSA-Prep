062885fb2d7b8ef0a7df90c84653041730d05cd9
TehAutumnCore <naruto13581@gmail.com>
2025-05-22 23:45:07 -0400

    completed cpp template and starting first cpp program

---
+++ Learning_C++/FirstCPPProgram/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //standard input/output library
+
+int main() { //entry point of the program
+    std::cout << "Hello, C++!" << std::endl; //std is the namespace, cout is the standard output stream object in the std namespace, << is the stream insertion operator, and endl is like \n or writeline()
+    return 0; 
+}
