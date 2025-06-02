0e8c43122740bec1f959b3f531e9c0413fed6109
TehAutumnCore <naruto13581@gmail.com>
2025-06-01 20:25:16 -0400

    completed guided project follow up challenge unguided

--- MSSA-SWEFoundations/Section3_GetStarted_CSharp_Part3/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ MSSA-SWEFoundations/Section3_GetStarted_CSharp_Part3/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80c26fa9519014fd4d78a730e97415b4e186b46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f405c7a2725a4c8a49834a9ff287cd07322b3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
