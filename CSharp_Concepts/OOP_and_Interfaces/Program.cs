07d1dc57c6dcbf3ab82d5e7d071178bf37544af2
TehAutumnCore <naruto13581@gmail.com>
2025-06-08 17:01:33 -0400

    OOP and Interfaces before ASPNET

---
+++ CSharp_Concepts/OOP_and_Interfaces/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClasses
+{
+    class Animal
+    {
+        string name{ get;set;}
+        bool isAlive{get;set;}
+        int weight{get;set;}
+        public Animal()
+        {
+            Console.WriteLine("Animal Constructor");
+        }
+
+        public Animal(string name,bool isAlive, int weight)
+        {
+            this.name = name;
+            this.isAlive = isAlive;
+            this.weight = weight;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Animal said 'hello'");
+        }
+        public void Talk()
+        {
+            Console.WriteLine("Animal is talking");
+        }
+        public void Sing()
+        {
+            Console.WriteLine("Animal is singing");
+        }
+        public string ToString() {
+            return $"Animal ToString(): name: {name}\tStatus: {isAlive},\tWeight: {weight}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal beast = new Animal("beast",true, 100);
+
+            beast.Greet();
+            beast.Talk();
+            beast.Sing();
+
+            Console.WriteLine(beast.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_Concepts/readme.md
@@ -0,0 +1,16 @@
+Going through a youtube playlist to learn concepts and apply then. 
+
+C# Object Oriented Programming 01 class properties methods
+https://www.youtube.com/watch?v=ObHutQBh1Do&list=PLoTYPeykRcoYd2oaK4GnLtz995s_Fl5YR&index=1
+
+C# Object Oriented Programming 02 dog
+https://www.youtube.com/watch?v=yRRxAn_r1vc&list=PLoTYPeykRcoYd2oaK4GnLtz995s_Fl5YR&index=2
+
+C# Object Oriented Programming 03 interface
+https://www.youtube.com/watch?v=pQeM0Jtuj3U&list=PLoTYPeykRcoYd2oaK4GnLtz995s_Fl5YR&index=3
+
+Super EASY Todo List ASP.NET C# MVC Beginners
+https://www.youtube.com/watch?v=ZypiARkybY0&list=PLoTYPeykRcoYd2oaK4GnLtz995s_Fl5YR&index=4
+
+Build a RESTful API in ASP.NET
+https://www.youtube.com/watch?v=Tj3qsKSNvMk&list=PLoTYPeykRcoYd2oaK4GnLtz995s_Fl5YR&index=5
